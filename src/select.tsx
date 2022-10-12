6d7258a837f6ec0163fa218f1706d0f38cadffc3
code.bryan <brya0196@gmail.com>
2022-10-12 12:09:05 -0400

    feat: include tailwindcss

--- src/select.tsx
+++ src/select.tsx
@@ -1,3 +1,32 @@
-export default function Select() {
-  
+import { forwardRef, useRef } from "react";
+
+export interface BaseSelectRef<O = any> {
+  open: boolean;
+  text: string;
+  value?: O;
+  openDropdown: () => void;
+  clearInput: () => void;
 }
+
+export function useSelect() {
+  return {
+    model: {},
+    operations: {},
+  };
+}
+
+const Select = forwardRef<BaseSelectRef>(() => {
+  const divRef = useRef<HTMLDivElement>(null);
+
+  const {} = useSelect();
+
+  return (
+    <div ref={divRef}>
+      <div>
+        
+      </div>
+    </div>
+  );
+});
+
+export default Select;
